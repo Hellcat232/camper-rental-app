b83caf93f9944c523f6ae8024c15eccf27d7c167
Hellcat232 <maxkuich1993@gmail.com>
2024-08-21 03:02:40 +0300

    add 2 comonents into side bar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,16 +1,7 @@
 import css from "./SideBar.module.css";
-import clsx from "clsx";
-import { MdOutlineAir } from "react-icons/md";
-import { TbToolsKitchen2 } from "react-icons/tb";
-import { GiTv } from "react-icons/gi";
-import { LiaShowerSolid } from "react-icons/lia";
-import { TbAutomaticGearbox } from "react-icons/tb";
-import { TbCaravan } from "react-icons/tb";
+import VehicleType from "../VehicleType/VehicleType";
+import FilterToEquipment from "../FilterToEquipment/FilterToEquipment";
 import { useState } from "react";
-
-const buildLinkClass = ({ isActive }) => {
-  return clsx(css["equip-black"], isActive && css["equip-red"]);
-};
 
 const SideBar = ({ filter }) => {
   const [filtersValue, setFiltersValue] = useState({
@@ -22,8 +13,6 @@ const SideBar = ({ filter }) => {
     airConditioner: false,
     form: "",
   });
-
-  console.log(filtersValue, "filtersValue");
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -79,127 +68,14 @@ const SideBar = ({ filter }) => {
         </div>
 
         <>
-          <div style={{ marginTop: "32px" }}>
-            <p>Filters</p>
-            <h3 className={css["vehicle-equipment-title"]}>
-              Vehicle equipment
-            </h3>
-
-            <ul className={css["equip-list"]}>
-              <li className={css.equip}>
-                <input
-                  type="checkbox"
-                  id="airConditioner"
-                  name="airConditioner"
-                  checked={filtersValue.airConditioner}
-                  onChange={handleCheckboxChange}
-                />
-                <label htmlFor="airConditioner" className={css.label}>
-                  <MdOutlineAir className={css["equip-icon"]} /> AC
-                </label>
-              </li>
-
-              <li className={css.equip}>
-                <input
-                  type="checkbox"
-                  id="transmission"
-                  name="transmission"
-                  // value="Automatic"
-                  checked={filtersValue.transmission}
-                  onChange={handleCheckboxChange}
-                />
-                <label htmlFor="transmission" className={css.label}>
-                  <TbAutomaticGearbox className={css["equip-icon"]} /> Automatic
-                </label>
-              </li>
-              <li className={css.equip}>
-                <input
-                  type="checkbox"
-                  id="kitchen"
-                  name="kitchen"
-                  checked={filtersValue.kitchen}
-                  onChange={handleCheckboxChange}
-                />
-                <label htmlFor="kitchen" className={css.label}>
-                  <TbToolsKitchen2 className={css["equip-icon"]} /> Kitchen
-                </label>
-              </li>
-              <li className={css.equip}>
-                <input
-                  type="checkbox"
-                  id="tv"
-                  name="tv"
-                  checked={filtersValue.tv}
-                  onChange={handleCheckboxChange}
-                />
-                <label htmlFor="tv" className={css.label}>
-                  <GiTv className={css["equip-icon"]} /> TV
-                </label>
-              </li>
-              <li className={css.equip}>
-                <input
-                  type="checkbox"
-                  id="shower"
-                  name="shower"
-                  checked={filtersValue.shower}
-                  onChange={handleCheckboxChange}
-                />
-                <label htmlFor="shower" className={css.label}>
-                  <LiaShowerSolid className={css["equip-icon"]} />
-                  Shower/WC
-                </label>
-              </li>
-            </ul>
-          </div>
-
-          <div style={{ marginTop: "32px" }}>
-            <h3 className={css["vehicle-type"]}>Vehicle type</h3>
-
-            <ul className={css["van-list"]}>
-              <li className={css.vans}>
-                <input
-                  type="radio"
-                  id="panelTruck"
-                  name="form"
-                  value="panelTruck"
-                  checked={filtersValue.form === "panelTruck"}
-                  onChange={handleVehicleTypeChange}
-                />
-                <label htmlFor="panelTruck" className={css.label}>
-                  <TbCaravan className={css["van-icon"]} />
-                  Van
-                </label>
-              </li>
-              <li className={css.vans}>
-                <input
-                  type="radio"
-                  id="fullyIntegrated"
-                  name="form"
-                  value="fullyIntegrated"
-                  checked={filtersValue.form === "fullyIntegrated"}
-                  onChange={handleVehicleTypeChange}
-                />
-                <label htmlFor="fullyIntegrated" className={css.label}>
-                  <TbCaravan className={css["van-icon"]} />
-                  Integrated
-                </label>
-              </li>
-              <li className={css.vans}>
-                <input
-                  type="radio"
-                  id="alcove"
-                  name="form"
-                  value="alcove"
-                  checked={filtersValue.form === "alcove"}
-                  onChange={handleVehicleTypeChange}
-                />
-                <label htmlFor="alcove" className={css.label}>
-                  <TbCaravan className={css["van-icon"]} />
-                  Alcove
-                </label>
-              </li>
-            </ul>
-          </div>
+          <FilterToEquipment
+            filtersValue={filtersValue}
+            handleCheckboxChange={handleCheckboxChange}
+          />
+          <VehicleType
+            filtersValue={filtersValue}
+            handleVehicleTypeChange={handleVehicleTypeChange}
+          />
         </>
 
         <button className={css["search-btn"]}>Search</button>
